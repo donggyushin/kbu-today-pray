42a7f28cf85511fad3cdb464dfc29ee0be70ede3
shin donggyu <donggyu9410@gmail.com>
2020-02-08 15:50:48 +0900

    asd

--- src/consts/endPoint.ts
+++ src/consts/endPoint.ts
@@ -4,6 +4,6 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     // dev code
     DONGGYU_ENDPOINT = 'http://localhost:4001/api/'
 } else {
-    DONGGYU_ENDPOINT = '220.67.154.77:4001/api/'
+    DONGGYU_ENDPOINT = 'https://kbucard.com:4001/api/'
 }
 
